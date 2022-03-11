b04a41e1e68654205713a5498ce42a2ce6d04b1d
Rashidul Hasan <rhratul12@gmail.com>
2022-03-12 04:30:24 +0600

    Search Product assertion ,Open Menu Assertion and Many more

--- cypress/integration/home-page-test-cases/homePageTest.spec.js
+++ cypress/integration/home-page-test-cases/homePageTest.spec.js
@@ -10,7 +10,7 @@ describe("Homepage Test", () => {
   before(() => {
     //Visit the url
     cy.visit(url);
-    // welcomeMessage Popup	close 
+    // welcomeMessage Popup	close
     homepage.welcomeMessage().click();
   });
   it("Should verify Logo is visible or not", () => {
@@ -19,27 +19,48 @@ describe("Homepage Test", () => {
 
   it("Should verify Menu button is visible or not", () => {
     homepage.menuButton().should("exist");
+    homepage.menuButton().click();
+    cy.wait(1000);
+    homepage
+      .menuButton()
+      .should("have.class", ".sidebar-menu__container");
+         homepage.menuButton().click();
   });
 
   it("Should verify Account button is visible or not", () => {
     homepage.accountButton().should("exist");
   });
 
-  it("Should verify Search box is visible or not", () => {
+  it("Should verify Search box is visible and Functionality is visible and works or not", () => {
     homepage.searchBox().should("exist");
+    homepage.searchBox().click();
+    cy.wait(1000);
+    homepage.modalSearchBox().type("Mobile");
+    cy.wait(2000);
+    homepage.searchField().should("have.length", 1);
+    homepage.searchField().should("contain", "Mobile");
+    cy.contains("Mobile");
+    homepage.modalSearchBox().clear();
+    homepage.closeSearchPanel().click();
   });
 
-  it("Should verify Hot Deals is present", () => { 
+  it("Should verify Hot Deals is present", () => {
     homepage.campaignSection().contains("Hot Deals").should("exist");
-  })
+  });
 
-  it("Should verify Trending Accessories is present", () => {  
+  it("Should verify Trending Accessories is present", () => {
     homepage.campaignSection().contains("Trending Accessories").should("exist");
-  })
+  });
 
-  it("Should verify Top Brands is present", () => {  
-    homepage.topBrands().contains("Top Brands").should("exist");
-  })
+  it("Should verify Trending Mobile Phones is present", () => {
+    homepage.campaignSection().contains("Trending Mobile Phones").should(
+      "exist"
+    );    
+  });
+
+  it("Should verify Category of the Day is present", () => {
+    homepage.campaignSection().contains("Category of the Day").should("exist");
+  });
 
   it("Should verify Trending Mobile Phones is present", () => {
     homepage
@@ -48,4 +69,5 @@ describe("Homepage Test", () => {
       .should("exist");
   });
 
+  
 });
